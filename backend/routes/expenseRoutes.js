843d40f8050077be3b86088664ebb6b7e7de2e23
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-10-24 12:54:29 +0100

    authenticated routes

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const router = express.Router();
 
 const Expense = require('../models/expense.model');
+const authUser = require('../middlewares/auth');
 
 //Get all expenses
-router.get('/',(req,res)=>{
+router.get('/',authUser,(req,res)=>{
     Expense.find().then(expenses => res.json(expenses)).catch(err=> res.status(400).json(`Error:${err}`));
 });
 
 //Create an expense
-router.post('/create',(req,res)=>{
+router.post('/create',authUser,(req,res)=>{
     const {description, payment,category,amount }= req.body;
     const date = Date.parse(req.body.date);
     const newExpense = new Expense({
@@ -30,7 +31,7 @@ router.post('/create',(req,res)=>{
 });
 
 //Updated expense
-router.put('/expense/update/:id',(req,res)=>{
+router.put('/expense/update/:id',authUser,(req,res)=>{
     Expense.findById(req.params.id)
     .then(expense =>{
         expense.description = req.body.description;
@@ -56,7 +57,7 @@ router.put('/expense/update/:id',(req,res)=>{
 // });
 
 //Delete expense by id
-router.delete('/expense/:id',(req,res)=>{
+router.delete('/expense/:id',authUser,(req,res)=>{
     Expense.findByIdAndDelete(req.params.id)
     .then(()=> res.status(200).json('Expense Deleted'))
     .catch(err => res.status(404).json(err))
